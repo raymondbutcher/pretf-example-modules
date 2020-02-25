ba5d2954da8bb48ba5ae44c81e98d4e92b332443
Raymond Butcher <ray.butcher@claranet.uk>
2020-02-25 15:23:42 +0000

    Add third variable to pass module

--- pass/main.tf
+++ pass/main.tf
@@ -2,6 +2,10 @@ variable "first" {}
 
 variable "second" {}
 
+variable "third" {}
+
 output "first" { value = var.first }
 
 output "second" { value = var.second }
+
+output "third" { value = var.third }
